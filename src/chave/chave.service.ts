1fafb671ad6cab0c31e7a0a0c08c7d4a27de5bf4
Leonardorossato <leonardorossato21@gmail.com>
2023-07-16 09:58:48 -0300

    update chave.service.ts

--- src/chave/chave.service.ts
+++ src/chave/chave.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateChaveDto } from './dto/create-chave.dto';
 import { UpdateChaveDto } from './dto/update-chave.dto';
+import { Chave } from './entities/chave.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ChaveService {
+  constructor(
+    @InjectRepository(Chave)
+    private readonly chaveNfeRepository: Repository<Chave>,
+  ) {}
+
   create(createChaveDto: CreateChaveDto) {
     return 'This action adds a new chave';
   }
 
-  findAll() {
-    return `This action returns all chave`;
+  async findAll() {
+    try {
+      const chave = await this.chaveNfeRepository.find();
+      return chave;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao tentar achar todas as chaves NF-e',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} chave`;
-  }
+  async findOne(id: number) {}
 
-  update(id: number, updateChaveDto: UpdateChaveDto) {
-    return `This action updates a #${id} chave`;
-  }
+  async update(id: number, updateChaveDto: UpdateChaveDto) {}
 
-  remove(id: number) {
-    return `This action removes a #${id} chave`;
-  }
+  async remove(id: number) {}
 }
