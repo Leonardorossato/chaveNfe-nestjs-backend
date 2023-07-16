771f2d66e3b894d654c014566c5e1b6257d63204
Leonardorossato <leonardorossato21@gmail.com>
2023-07-16 09:33:09 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresSqlConnection } from './config/ormconfig';
+import { ChaveModule } from './chave/chave.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(PostgresSqlConnection),
+    ChaveModule,
+    
   ],
 })
 export class AppModule {}
