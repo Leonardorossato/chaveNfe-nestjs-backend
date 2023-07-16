bee801a50004dffe7d2bdc0156d4b01a9617c62b
Leonardorossato <leonardorossato21@gmail.com>
2023-07-16 09:58:45 -0300

    update chave.controller.ts

--- src/chave/chave.controller.ts
+++ src/chave/chave.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ChaveService } from './chave.service';
 import { CreateChaveDto } from './dto/create-chave.dto';
 import { UpdateChaveDto } from './dto/update-chave.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('chave')
+@ApiTags('Chave NF-e')
 export class ChaveController {
   constructor(private readonly chaveService: ChaveService) {}
 
-  @Post()
-  create(@Body() createChaveDto: CreateChaveDto) {
+  @Post('/create')
+  async create(@Body() createChaveDto: CreateChaveDto) {
     return this.chaveService.create(createChaveDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('/all')
+  async findAll() {
     return this.chaveService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: number) {
     return this.chaveService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChaveDto: UpdateChaveDto) {
+  async update(
+    @Param('id') id: number,
+    @Body() updateChaveDto: UpdateChaveDto,
+  ) {
     return this.chaveService.update(+id, updateChaveDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: number) {
     return this.chaveService.remove(+id);
   }
 }
