4d39a0c358afaff63fcc624ce05341803d183686
Leonardorossato <leonardorossato21@gmail.com>
2023-07-16 09:58:31 -0300

    update chave.entity.ts

--- src/chave/entities/chave.entity.ts
+++ src/chave/entities/chave.entity.ts
@@ -1,1 +1,34 @@
-export class Chave {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Chave {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({nullable: false, length: 2})
+  uf: string;
+
+  @Column({nullable: false, length: 4})
+  dataDeEmissao: string;
+
+  @Column({nullable: false, length: 14})
+  cnpj: string;
+
+  @Column({nullable: false, length: 2})
+  modeloNFe: string;
+
+  @Column({nullable: false, length: 3})
+  serie: string;
+
+  @Column({nullable: false, length: 9})
+  numeroNfe: number;
+
+  @Column({nullable: false, length: 2})
+  tipoEmissao: string;
+
+  @Column({nullable: false, length:8})
+  codNumerico: string;
+
+  @Column()
+  cDV?: string;
+}
