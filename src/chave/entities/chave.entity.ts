c499c856d7d637f0fb12060ffaf21abaecbe904a
Leonardorossato <leonardorossato21@gmail.com>
2023-07-16 10:24:59 -0300

    correção

--- src/chave/entities/chave.entity.ts
+++ src/chave/entities/chave.entity.ts
@@ -5,28 +5,28 @@ export class Chave {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({nullable: false, length: 2})
+  @Column({ nullable: false, length: 2 })
   uf: string;
 
-  @Column({nullable: false, length: 4})
+  @Column({ nullable: false, length: 4 })
   dataDeEmissao: string;
 
-  @Column({nullable: false, length: 14})
+  @Column({ nullable: false, length: 14 })
   cnpj: string;
 
-  @Column({nullable: false, length: 2})
+  @Column({ nullable: false, length: 2 })
   modeloNFe: string;
 
-  @Column({nullable: false, length: 3})
+  @Column({ nullable: false, length: 3 })
   serie: string;
 
-  @Column({nullable: false, length: 9})
-  numeroNfe: number;
+  @Column({ nullable: false, length: 9 })
+  numeroNfe: string;
 
-  @Column({nullable: false, length: 2})
+  @Column({ nullable: false, length: 2 })
   tipoEmissao: string;
 
-  @Column({nullable: false, length:8})
+  @Column({ nullable: false, length: 8 })
   codNumerico: string;
 
   @Column()
