7ee5016063f7dd42046e0f7547c4d3941fd49cf9
Leonardorossato <leonardorossato21@gmail.com>
2023-08-04 15:25:39 -0300

    update chave.entity.ts

--- src/chave/entities/chave.entity.ts
+++ src/chave/entities/chave.entity.ts
@@ -28,54 +28,75 @@ export class Chave {
   @Column({ nullable: false })
   codNumerico: number;
 
-  @Column()
+  @Column({ nullable: true })
   chaveNfe?: string;
 
   gerarChaveNfe(): void {
-    const ufNumerico = this.geUfCodigo(this.uf);
+    if (
+      !this.uf ||
+      !this.dataDeEmissao ||
+      !this.cnpj ||
+      !this.modeloNFe ||
+      !this.serie ||
+      !this.numeroNfe ||
+      !this.tipoEmissao ||
+      !this.codNumerico
+    ) {
+      throw new Error(
+        'Alguma propriedade necessária para gerar a chave está vazia.',
+      );
+    }
 
-    // Remova qualquer formatação do CNPJ (pontos, traços e barras) e converta para um número
-    const cnpjNumerico = parseInt(this.cnpj.replace(/[.-\/]/g, ''), 10);
+    // Verificar se as propriedades numéricas (serie, numeroNfe e codNumerico) são válidas
+    if (isNaN(this.serie) || isNaN(this.numeroNfe) || isNaN(this.codNumerico)) {
+      throw new Error(
+        'Alguma propriedade numérica necessária para gerar a chave é inválida.',
+      );
+    }
 
-    // Conversão do modeloNfe para um número
-    const modeloNfeNumerico = this.getNotaFiscal(this.modeloNFe);
-
-    // Conversão do tipoDeEmissao para um número
+    const data = this.formatDate(this.dataDeEmissao);
+    const numero = this.numeroNfe.toString();
+    const ufNumerico = this.geUfCodigo(this.uf).toString();
+    const cnpjNumerico = this.cnpj.replace(/\D/g, '');
+    const modeloNfeNumerico = this.getNotaFiscal(this.modeloNFe).toString();
     const tipoDeEmissaoNumerico = this.getTipoDeEmissao(this.tipoEmissao);
+    const serie = this.serie.toString();
+    const codNumerico = this.codNumerico.toString();
 
-    const chaveSemDigito = `${ufNumerico}${this.dataDeEmissao}${cnpjNumerico}${modeloNfeNumerico}${this.serie}${this.numeroNfe}${tipoDeEmissaoNumerico}${this.codNumerico}`;
+    const chaveSemDigito = `${ufNumerico}${data}${cnpjNumerico}${modeloNfeNumerico}${serie}${numero}${tipoDeEmissaoNumerico}${codNumerico}`;
     const dvCalculado = this.calcularDigitoVerificador(chaveSemDigito);
     this.chaveNfe = `${chaveSemDigito}${dvCalculado}`;
   }
 
   private geUfCodigo(uf: string) {
-    const ufMapping: { [key: string]: string } = {
-      AC: '12',
-      AL: '27',
-      AM: '13',
-      AP: '16',
-      BA: '29',
-      CE: '23',
-      DF: '53',
-      ES: '32',
-      GO: '52',
-      MA: '21',
-      MG: '31',
-      MS: '50',
-      MT: '51',
-      PA: '15',
-      PB: '25',
-      PE: '26',
-      PI: '22',
-      PR: '41',
-      RJ: '33',
-      RN: '24',
-      RO: '11',
-      RR: '14',
-      RS: '43',
-      SC: '42',
-      SE: '28',
-      SP: '35',
+    const ufMapping: { [key: string]: number } = {
+      AC: 12,
+      AL: 27,
+      AM: 13,
+      AP: 16,
+      BA: 29,
+      CE: 23,
+      DF: 53,
+      ES: 32,
+      GO: 52,
+      MA: 21,
+      MG: 31,
+      MS: 50,
+      MT: 51,
+      PA: 15,
+      PB: 25,
+      PE: 26,
+      PI: 22,
+      PR: 41,
+      RJ: 33,
+      RN: 24,
+      RO: 11,
+      RR: 14,
+      RS: 43,
+      SC: 42,
+      SE: 28,
+      SP: 35,
+      TO: 17,
     };
     const ufNumerico = ufMapping[uf];
     if (!ufNumerico) {
@@ -84,11 +105,11 @@ export class Chave {
     return ufNumerico;
   }
   private getNotaFiscal(nFE: string) {
-    const numeroNotaFdical: { [key: string]: string } = {
-      NFe: '55',
-      MDFe: '58',
-      NFCe: '65',
-      CTe: '57',
+    const numeroNotaFdical: { [key: string]: number } = {
+      NFe: 55,
+      MDFe: 58,
+      NFCe: 65,
+      CTe: 57,
     };
     const numericoFsical = numeroNotaFdical[nFE];
     if (!numericoFsical) {
@@ -98,14 +119,14 @@ export class Chave {
   }
 
   private getTipoDeEmissao(tpm: string) {
-    const tipoDeEmissao: { [key: string]: string } = {
-      Normal: '1',
-      ContigenciaFs: '2',
-      ContigenciaSCAN: '3',
-      ContigenciaDPEC: '4',
-      ContigenciaFSDA: '5',
-      ContigenciaSVCAN: '6',
-      ContigenciaSCVRS: '7',
+    const tipoDeEmissao: { [key: string]: number } = {
+      Normal: 1,
+      ContigenciaFs: 2,
+      ContigenciaSCAN: 3,
+      ContigenciaDPEC: 4,
+      ContigenciaFSDA: 5,
+      ContigenciaSVCAN: 6,
+      ContigenciaSCVRS: 7,
     };
 
     const conversaoEmissao = tipoDeEmissao[tpm];
@@ -115,24 +136,33 @@ export class Chave {
     return conversaoEmissao;
   }
 
-  private calcularDigitoVerificador(chaveSemDigito: string): string {
+  private formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear().toString();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}${month}${day}`;
+  }
+
+  private calcularDigitoVerificador(chaveSemDigito: string) {
     let soma = 0;
-    const multiplicadores = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9];
+    const multiplicadores = [
+      2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9, 2,
+      3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9,
+    ];
 
     for (let i = chaveSemDigito.length - 1; i >= 0; i--) {
       const digito = parseInt(chaveSemDigito.charAt(i), 10);
-      soma +=
-        digito *
-        multiplicadores[multiplicadores.length - chaveSemDigito.length + i];
+      const multiplicador = multiplicadores[i];
+      console.log(`Digito: ${digito}, Multiplicador: ${multiplicador}`);
+      soma += digito * multiplicador;
     }
-
     const resto = soma % 11;
     let dvCalculado = 11 - resto;
 
     if (dvCalculado === 0 || dvCalculado === 1) {
       dvCalculado = 0;
     }
-
     return dvCalculado.toString();
   }
 }
